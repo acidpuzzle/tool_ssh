0b63c69dfa12feab71530c8196af29774d475bb0
Pavlov <acidpuzzle@mail.ru>
2022-08-12 12:13:20 +0300

    Added `env_var` argument to function `netmiko_dev`

--- ssh.py
+++ ssh.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import os
 
 from netmiko import *
 from ipaddress import ip_address
@@ -7,6 +8,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 def netmiko_dev(*,
@@ -15,6 +17,7 @@ def netmiko_dev(*,
                 username: str = None,
                 password: str = None,
                 enable_pass: str = "cisco",
+                env_var: str = None,
                 ) -> dict:
     """
     :param device_type: Netmiko platform, example "cisco_ios", "huawei", "cisco_ios_telnet"
@@ -23,8 +26,11 @@ def netmiko_dev(*,
     :param username:
     :param password:
     :param enable_pass:
+    :param env_var:
     :return: Dictionary with device connection parameters
     """
+    if env_var:
+        username, password, enable_pass = os.environ.get(env_var).split(':')
     return {
         'device_type': device_type,
         'host': device_ip,
@@ -32,6 +38,22 @@ def netmiko_dev(*,
         'password': password,
         'secret': enable_pass,
     }
+
+
+def cisco_ios_ssh(device_ip: str, **kwargs) -> dict:
+    """
+    :param device_ip: ip address in textual representation, for example "192.168.1.1"
+    :return: Dictionary with device connection parameters
+    """
+    return netmiko_dev(device_type="cisco_ios", device_ip=device_ip, **kwargs)
+
+
+def cisco_ios_telnet(device_ip: str, **kwargs) -> dict:
+    """
+    :param device_ip: ip address in textual representation, for example "192.168.1.1"
+    :return: Dictionary with device connection parameters
+    """
+    return netmiko_dev(device_type="cisco_ios_telnet", device_ip=device_ip, **kwargs)
 
 
 def get_config(device: dict, command: str) -> str:
@@ -68,6 +90,7 @@ def send_config(device: dict, commands: list) -> str:
     try:
         with ConnectHandler(**device) as ssh:
             out = ssh.send_config_set(commands)
+            ssh.save_config()
             return out
     except (
             NetmikoAuthenticationException,
@@ -78,7 +101,7 @@ def send_config(device: dict, commands: list) -> str:
         logger.error(err.__str__)
 
 
-def run_in_threads(worker, max_thread=60, *args, **kwargs):
+def run_in_threads(worker, max_thread=60, *args):
     """
     len(all_devices) == len(all_commands) = True
     :param worker: a function that will run in threads
@@ -86,7 +109,7 @@ def run_in_threads(worker, max_thread=60, *args, **kwargs):
     :return:
     """
     with ThreadPoolExecutor(max_workers=max_thread) as executor:
-        results = executor.map(worker, *args, **kwargs)
+        results = executor.map(worker, *args)
 
     return results
 
